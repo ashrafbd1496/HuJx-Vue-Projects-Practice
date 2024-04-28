67773803c60db613267b87322e8f41f79420f2c0
ashrafbd1496 <asraf1496@gmail.com>
2024-04-28 11:52:03 +0600

    Random quotes generator using api done

--- src/App-Githubuserprofile.vue
+++ src/App-Githubuserprofile.vue
@@ -5,14 +5,12 @@
 </template>
 <script setup>
 import { ref } from "vue";
-import GithubUserSearch from "./components/RandomNumber.vue";
+import GithubUserSearch from "./components/GithubUserSearch.vue";
 </script>
 <style>
 body,html {
   margin: 0;
   padding: 0;
-  background-color: #008080;
-  color: #fff;
   
 }
 </style>
---
+++ src/components/RandomNumber.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const quote = ref({});
+
+const getQuotes = async () => {
+  try {
+    const response = await fetch(`https://api.quotable.io/quotes/random?tags=technology,famous-quotes`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch quote");
+    }
+    const data = await response.json();
+  
+    quote.value.content = data[0].content;
+    quote.value.author = data[0].author;
+  } catch (error) {
+    console.error("Error fetching quotes:", error);
+  }
+};
+
+// Call getQuotes function when component is mounted
+onMounted(getQuotes);
+
+const fetchNewQuote = () => {
+  getQuotes();
+};
+</script>
+
+<template>
+  <div class="quote-generator">
+    <h1 class="app-title">Random Quote Generator</h1>
+    <blockquote class="quote-container">
+      <p>"{{ quote.content }}"</p>
+      <cite class="quote-author">_ _{{ quote.author }}</cite>
+    </blockquote>
+    <button @click="fetchNewQuote" class="quote-button">
+      Get Random Quote
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.quote-generator {
+  border: solid 1px #fff;
+  padding: 20px;
+  display: grid;
+  place-items: center end;
+}
+.quote-generator .app-title {
+  border-bottom: 1px dashed #fff;
+  margin-bottom: 20px;
+}
+.quote-container > cite {
+  display: grid;
+  text-align: right;
+  margin-top: 10px;
+}
+.quote-generator .quote-button {
+  display: inline-block;
+  width: 150px;
+  align-self: self-end;
+  margin-top: 20px;
+  padding: 5px;
+}
+</style>
