22a13ad04551a260000327eb75ea27c0a8898a21
ashrafbd1496 <asraf1496@gmail.com>
2024-05-01 22:00:58 +0600

    calculator project done

--- src/App-RandomQuote.vue
+++ src/App-RandomQuote.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <Calculator />
+    <GithubUserSearch />
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
-import Calculator from "./components/Calculator.vue";
+import GithubUserSearch from "./components/RandomNumber.vue";
 </script>
 <style>
-body,
-html {
+body,html {
   margin: 0;
   padding: 0;
   background-color: #008080;
   color: #fff;
+  
 }
 </style>
